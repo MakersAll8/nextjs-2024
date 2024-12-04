83b1a2b3d32d6cc6a3eaeaf8a4371242f5b770c1
Xiao Li <me@xiao.engineer>
2024-12-04 09:38:57 +0800

    lint

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -57,6 +57,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
   } catch (error) {
+    console.log({ error });
     return {
       message: "Database Error: Failed to Create Invoice.",
     };
@@ -86,6 +87,7 @@ export async function updateInvoice(
         WHERE id = ${id}
       `;
   } catch (error) {
+    console.log({ error });
     return { message: "Database Error: Failed to Update Invoice." };
   }
 
@@ -100,6 +102,7 @@ export async function deleteInvoice(id: string) {
     revalidatePath("/dashboard/invoices");
     return { message: "Deleted Invoice." };
   } catch (error) {
+    console.log({ error });
     return { message: "Database Error: Failed to Delete Invoice." };
   }
 }
